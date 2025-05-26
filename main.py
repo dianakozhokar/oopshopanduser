337cd4fe2112c3a9c4b65f4f185a5c5dc9182108
dianakozhokar <kozhokar.diana@chnu.edu.ua>
2025-05-26 23:11:31 +0300

    1

--- main.py
+++ main.py
@@ -1,4 +1,41 @@
-from shop_module import Shop
+from shop_module import Shop, Discount
 
-all_store = Shop("GlobalStore", "General")
-all_store.describe_shop()
+
+store = Shop("ElectroShop", "Electronics")
+print(store.shop_name)
+print(store.store_type)
+store.describe_shop()
+store.open_shop()
+
+print()
+
+
+store1 = Shop("BookWorld", "Books")
+store2 = Shop("StyleHub", "Clothing")
+store3 = Shop("FreshMart", "Groceries")
+
+store1.describe_shop()
+store2.describe_shop()
+store3.describe_shop()
+
+print()
+
+
+print(f"Default number of units in store: {store.number_of_units}")
+store.number_of_units = 20
+print(f"Updated number of units: {store.number_of_units}")
+
+print()
+
+
+store.set_number_of_units(35)
+print(f"Number of units after setting: {store.number_of_units}")
+
+store.increment_number_of_units(5)
+print(f"Number of units after incrementing: {store.number_of_units}")
+
+print()
+
+
+store_discount = Discount("FashionSale", "Clothing", ["Jeans", "T-Shirts", "Sneakers"])
+store_discount.get_discounts_products()
